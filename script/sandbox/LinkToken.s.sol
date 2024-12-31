7b4436de0c0e33374e324e291f405f4975880a3e
kmtr <kmtr@outlook.com>
2024-12-31 10:54:16 +0900

    explicitly mint LinkToken to deployer

--- script/sandbox/LinkToken.s.sol
+++ script/sandbox/LinkToken.s.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.2 <0.9.0;
 
 import "forge-std/Script.sol";
 import "src/interfaces/shared/LinkTokenInterface.sol";
+import "@chainlink/contracts/v0.8/shared/token/ERC677/LinkToken.sol";
 
 contract LinkTokenScript is Script {
   function run() external view {
@@ -15,6 +16,11 @@ contract LinkTokenScript is Script {
     vm.startBroadcast(deployerPrivateKey);
 
     address linkToken = deployCode("LinkToken.sol:LinkToken");
+
+    LinkToken lt = LinkToken(linkToken);
+    address deployer = vm.addr(deployerPrivateKey);
+    lt.grantMintAndBurnRoles(deployer);
+    lt.mint(deployer, 1000000000000000000000);
 
     vm.stopBroadcast();
 
